d264b693a289b55d5879a204f4b3cc0eeaf15a60
itsjohnong <itsjohnong@gmail.com>
2021-01-12 14:22:57 -0800

    ready for deploy

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quotes from './Quotes';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <Quotes />
+        </div>
+    );
+};
+
+export default App;
